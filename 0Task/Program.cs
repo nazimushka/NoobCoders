17cb3ca872755a88ad791ce00264fe68428f381f
Gasanov Nazim <nazimushka@gmail.com>
2022-10-28 01:55:52 +0300

    первая задача

---
+++ 0Task/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число:");
+string number = Console.ReadLine();
+
+int intNum = int.Parse(number);
+
+intNum++;
+intNum += 1;
+
+Console.WriteLine($"Теперь значение введённого числа равно: {intNum}");
+
+// Для того, чтобы программа не "схлапывалась сразу после вывода
+Console.Read();
